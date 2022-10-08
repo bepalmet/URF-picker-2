e56d1d6919a60bbbb03cdbb8b835d2fb2455703d
bepalmet <bpalme68@gmail.com>
2022-10-09 00:41:23 +0200

    Add files via upload

---
+++ updater.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  9 00:36:59 2022
+
+@author: Ben-Gaming
+"""
+
+import os
+import urllib.request
+
+path = '\\'.join(os.path.realpath(__file__).split('\\')[:-1]) + '\\'
+
+response = urllib.request.urlopen(
+    'https://raw.githubusercontent.com/bepalmet/URF-picker-2/main/URF picker 2.py'
+    ).read().decode('utf-8')
+
+with open(path, 'w') as file:
+    file.write(response)
