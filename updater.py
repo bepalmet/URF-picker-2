f88519c350e4166366d64141ceac722ed1ad92ac
bepalmet <bpalme68@gmail.com>
2022-10-09 15:41:02 +0200

    Update updater.py

--- updater.py
+++ updater.py
@@ -11,7 +11,7 @@ import urllib.request
 path = '\\'.join(os.path.realpath(__file__).split('\\')[:-1]) + '\\'
 
 response = urllib.request.urlopen(
-    'https://raw.githubusercontent.com/bepalmet/URF-picker-2/main/URF picker 2.pyw'
+    'https://raw.githubusercontent.com/bepalmet/URF-picker-2/main/URF_picker_2.pyw'
     ).read().decode('utf-8').replace('\n', '')
 
 with open(path, 'w') as file:
